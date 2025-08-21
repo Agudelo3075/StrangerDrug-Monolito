5e6a33fdf69871d2095dd388b88d43b2a7d0f627
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-08-21 14:59:15 -0500

    Agregando modelos de las tres apis

--- strangerDrug/src/main/java/com/example/strangerDrug/verificacionDeCodigo/model/VerificationCode.java
+++ strangerDrug/src/main/java/com/example/strangerDrug/verificacionDeCodigo/model/VerificationCode.java
@@ -1,5 +1,59 @@
 package com.example.strangerDrug.verificacionDeCodigo.model;
 
+import java.time.LocalDateTime;
+
 public class VerificationCode {
-    
+    private Long id;
+    private String email;
+    private String code;
+    private LocalDateTime generatedAt;
+    private boolean used;
+
+    public VerificationCode(Long id, String email, String code, LocalDateTime generatedAt, boolean used){
+        this.id = id;
+        this.email = email;
+        this.code = code;
+        this.generatedAt = generatedAt;
+        this.used = used;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public LocalDateTime getGeneratedAt() {
+        return generatedAt;
+    }
+
+    public void setGeneratedAt(LocalDateTime generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
 }
