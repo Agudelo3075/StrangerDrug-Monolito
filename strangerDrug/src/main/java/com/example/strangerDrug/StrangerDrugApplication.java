3c6aef7809a9e78f491a245e99f209a9b32b524b
Agudelo3075 <sebasagudelo1012@gmail.com>
2025-08-21 12:42:38 -0500

    Agregrando primeros archivos

---
+++ strangerDrug/src/main/java/com/example/strangerDrug/StrangerDrugApplication.java
@@ -0,0 +1,13 @@
+package com.example.strangerDrug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrangerDrugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrangerDrugApplication.class, args);
+	}
+
+}
---
+++ strangerDrug/src/test/java/com/example/strangerDrug/StrangerDrugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strangerDrug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrangerDrugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
